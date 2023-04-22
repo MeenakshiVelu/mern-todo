3d5add72e0bcd4545b7d21a954bafffed7588a35
Meenakshi Velu <79783199+MeenakshiVelu@users.noreply.github.com>
2023-04-22 15:26:49 -0700

    third update

--- server.js
+++ server.js
@@ -1,8 +1,16 @@
-require("dotenv").config()
+// require("dotenv").config()
 const express = require("express");
 const todosRoutes = require("./routes/todosRoutes.js")
 const mongoose = require("mongoose")
 const path = require("path")
+const dotenv = require("dotenv");
+const connectDb = require("./config/connectDB.js");
+
+//config dotenc file
+dotenv.config();
+
+//databse call
+connectDb();
 
 const app = express();
 
@@ -24,18 +32,20 @@ app.get("*", (req,res)=>{
     res.sendFile(path.join(__dirname, "./client/build/index.html"))
 })
 
-const PORT = 8080 || process.env.PORT
-mongoose.connect(process.env.MONGO_URI)
-    .then(()=>{
-        //listen fo requests
-        app.listen(PORT, () => {
-            console.log("Connected to db and listeneing on port 8080")        
-        })
+const PORT = process.env.PORT || 8080
+// mongoose.connect(process.env.MONGO_URI)
+//     .then(()=>{
+//         //listen fo requests
+//         app.listen(PORT, () => {
+//             console.log("Connected to db and listeneing on port 8080")        
+//         })
 
-    })
-    .catch((error)=>{
-        console.log(error)
-    })
+//     })
+//     .catch((error)=>{
+//         console.log(error)
+//     })
 
 
-
+    app.listen(PORT, () => {
+        console.log(`Server running on port ${PORT}`);
+      });
